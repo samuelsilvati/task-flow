a75020266276e748d11195cb8eb93f507beb53e0
Samuel Silva <samuel7silva@outlook.com>
2023-06-17 19:19:43 -0300

    feat: create header home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../assets/logo.svg'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <header className="flex items-center justify-center bg-slate-400">
+        <div className="flex max-w-7xl flex-grow items-center justify-between py-4">
+          <Image src={Logo} alt="logo" />
+          <div className="flex gap-3">
+            <Link href="/signup" className="text-gray-500 underline">
+              Cria conta
+            </Link>
+            <Link href="/auth" className="text-gray-500 underline">
+              Login
+            </Link>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
