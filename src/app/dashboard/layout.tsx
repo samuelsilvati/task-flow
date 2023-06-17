270de3b4de1bd6e23be6b215b312c7b72919bd06
Samuel Silva <samuel7silva@outlook.com>
2023-06-17 19:21:54 -0300

    feat: create dashboard page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,29 @@
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+import Image from 'next/image'
+import Logo from '../../assets/logo.svg'
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const token = cookies().has('token')
+  if (!token) redirect('/auth')
+
+  return (
+    <>
+      <div>
+        <header className="flex items-center justify-center bg-slate-400">
+          <div className="flex max-w-7xl flex-grow items-center justify-between p-4">
+            <Image src={Logo} alt="logo" />
+            <div className="flex gap-3">
+              <Link href="/logout" className="text-gray-500 underline">
+                Sair
+              </Link>
+            </div>
+          </div>
+        </header>
+        <main>{children}</main>
+      </div>
+    </>
+  )
+}
