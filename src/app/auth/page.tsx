0d3bfebbd9b0f7f056d8a10cb8a484bd3fa37471
Samuel Silva <samuel7silva@outlook.com>
2023-06-17 19:21:03 -0300

    fix: redirect page after login

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,22 +1,23 @@
 'use client'
 import { api } from '@/lib/api'
 import { FormEvent } from 'react'
-
 import Cookies from 'js-cookie'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
-export default function auth() {
+export default function Auth() {
+  const router = useRouter()
   async function handleAuth(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
-    console.log(formData.get('email'))
     try {
       const authResponse = await api.post('/auth', {
         email: formData.get('email'),
         password: formData.get('password'),
       })
       const token = authResponse.data.token
-
       Cookies.set('token', token, { expires: 7, path: '/' })
+      router.push('/dashboard')
     } catch (err) {
       console.log(err)
     }
@@ -24,12 +25,15 @@ export default function auth() {
 
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <form onSubmit={handleAuth} className="flex flex-col gap-3">
+      <form onSubmit={handleAuth} className="flex flex-col gap-3 text-gray-800">
         <input type="email" placeholder="E-mail" name="email" />
         <input type="password" placeholder="Senha" name="password" />
-        <button type="submit" className="border py-2">
+        <button type="submit" className="border py-2 text-gray-200">
           Login
         </button>
+        <Link href="/signup" className="text-gray-300 underline">
+          Criar conta
+        </Link>
       </form>
     </div>
   )
