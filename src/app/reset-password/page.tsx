10498164b18b8d9066240a55d456226e3525ce50
Samuel Silva <samuel7silva@outlook.com>
2023-06-22 22:56:19 -0300

    fix: return not found page when expires

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 import { api } from '@/lib/api'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { notFound, useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { z } from 'zod'
 import toast from '../components/Toast'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import Loading from '../components/Loaging'
+import Loading from '../components/Loading'
 import decode from 'jwt-decode'
 
 const createUserformSchema = z
@@ -67,18 +67,7 @@ export default function ResetPasswordPage() {
   if (token) {
     const expires: Expires = decode(token)
     const now = Math.floor(Date.now() / 1000)
-    if (now > expires.exp)
-      return (
-        <div className="bg-white py-5 text-center">
-          <p className="text-base font-semibold text-gray-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            Page not found
-          </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-        </div>
-      )
+    if (now > expires.exp) return notFound()
   }
 
   return (
