8662c21144f7fdd1b1844f4bf2b4ab5d9ec07597
Samuel Silva <samuel7silva@outlook.com>
2023-06-22 22:57:25 -0300

    feat: change dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react'
 import Image from 'next/image'
 import Logo from '../../assets/logo.svg'
 import { Profile } from '../components/Profile'
+import Link from 'next/link'
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   const token = cookies().has('token')
@@ -16,6 +17,20 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
           <div className="flex max-w-7xl flex-grow items-center justify-between p-4">
             <Image src={Logo} alt="logo" />
             <div className="flex gap-3">
+              <div className="flex gap-3 pr-4">
+                <Link href="/" className="text-black">
+                  Perfil
+                </Link>
+                <Link href="/" className="text-black">
+                  Categorias
+                </Link>
+                <Link href="/" className="text-black">
+                  Categorias
+                </Link>
+                <Link href="/" className="text-black">
+                  Categorias
+                </Link>
+              </div>
               <Profile />
               <a href="/api/auth/logout" className="text-gray-500 underline">
                 Sair
@@ -23,7 +38,13 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
             </div>
           </div>
         </header>
-        <main>{children}</main>
+        <main>
+          <div className="flex h-full w-full flex-col items-center ">
+            <div className="relative mt-4 flex w-full max-w-7xl flex-grow flex-col items-center justify-between bg-gray-200 p-4 text-lg">
+              {children}
+            </div>
+          </div>
+        </main>
       </div>
     </>
   )
