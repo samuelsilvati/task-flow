de089204afb99891584715295e52baa346b1e907
Samuel Silva <samuel7silva@outlook.com>
2023-06-25 20:07:20 -0300

    fix home header colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Logo from '../assets/logo.svg'
 export default function Home() {
   return (
     <div>
-      <header className="flex items-center justify-center border-b border-gray-50 bg-slate-100 dark:border-gray-500 dark:bg-gray-700">
+      <header className="flex items-center justify-center border-b border-gray-100 bg-gray-50 dark:border-gray-500 dark:bg-gray-700">
         <div className="flex max-w-7xl flex-grow items-center justify-between p-4">
           <Image src={Logo} alt="logo" />
           <div className="flex gap-3">
-            <Link href="/signup" className="underline dark:text-white">
+            <Link href="/signup" className="dark:text-white">
               Cria conta
             </Link>
-            <Link href="/signin" className="underline dark:text-white">
+            <Link href="/signin" className="dark:text-white">
               Login
             </Link>
           </div>
