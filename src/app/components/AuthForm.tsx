507d129cddbfb8c6a1e2131f869506e4c5716fe9
Samuel Silva <samuel7silva@outlook.com>
2023-06-26 20:37:30 -0300

    fix: return redirect page router

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import Loading from './Loading'
 import toast from './Toast'
 import axios from 'axios'
-// import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 const createUserformSchema = z.object({
   email: z.string().nonempty('E-mail obrigatório').email('E-mail inválido'),
@@ -18,7 +18,7 @@ type CreateUserFormData = z.infer<typeof createUserformSchema>
 
 export default function AuthForm() {
   const [isLoading, setIsLoading] = useState(false)
-  // const router = useRouter()
+  const router = useRouter()
 
   const {
     register,
@@ -33,7 +33,7 @@ export default function AuthForm() {
       .post('/api/auth', data)
       .then(() => {
         setIsLoading(false)
-        // router.push(`/dashboard`)
+        router.push(`/dashboard`)
       })
       .catch((error) => {
         if (error) {
