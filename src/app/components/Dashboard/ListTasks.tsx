93d5f5e370afd8b1333a81caa4514310745bd376
Samuel Silva <samuel7silva@outlook.com>
2023-06-26 09:09:32 -0300

    feat: add button to add tasks on mobile

--- src/app/components/Dashboard/ListTasks.tsx
+++ src/app/components/Dashboard/ListTasks.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import toast from '../Toast'
 import Loading from '../Loading'
+import { Plus } from 'lucide-react'
 
 interface task {
   id: number
@@ -45,13 +46,24 @@ export default function ListTasks() {
       <>
         <Link
           href="/dashboard/new-task"
-          className="right-4 self-start rounded bg-green-500 px-6 py-2 font-bold text-white transition-colors hover:bg-green-600"
+          className="hidden self-start rounded bg-green-500 px-6 py-2 font-bold text-white transition-colors hover:bg-green-600 lg:block"
         >
           Adicionar Tarefa
         </Link>
-        <p className="pt-3">
-          Não existem tarefas! Clique no botão acima para adicionar
+        <p className="hidden pt-3 lg:block">
+          Clique no botão acima para adicionar sua primeira tarefa
         </p>
+        <Link
+          href="/dashboard/new-task"
+          className="absolute bottom-6 right-6 z-30 block cursor-none self-start rounded-full bg-green-500 p-4 font-bold text-white transition-colors hover:bg-green-600 lg:hidden"
+        >
+          <Plus />
+        </Link>
+        <div className="flex h-[70vh] items-center justify-center">
+          <p className="block pt-3 lg:hidden">
+            Clique no + para adicionar sua primeira tarefa
+          </p>
+        </div>
       </>
     )
   }
@@ -60,9 +72,15 @@ export default function ListTasks() {
     <>
       <Link
         href="/dashboard/new-task"
-        className="right-4 self-start rounded bg-green-500 px-6 py-2 font-bold text-white transition-colors hover:bg-green-600"
+        className="hidden self-start rounded bg-green-500 px-6 py-2 font-bold text-white transition-colors hover:bg-green-600 lg:block"
       >
         Adicionar Tarefa
+      </Link>
+      <Link
+        href="/dashboard/new-task"
+        className="absolute bottom-6 right-6 z-30 block cursor-none self-start rounded-full bg-green-500 p-2 font-bold text-white transition-colors hover:bg-green-600 lg:hidden"
+      >
+        <Plus />
       </Link>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {tasksData.map((task) => {
