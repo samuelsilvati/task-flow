84ca0cfb376590e9a3196b5bca316ab0ed59aa50
Samuel Silva <samuel7silva@outlook.com>
2023-07-02 14:36:26 -0300

    fix: eslint errors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,9 @@
-import AuthForm from "../components/AuthForm";
+import AuthForm from '../components/AuthForm'
 
-export default function SignIn () {
-  return <><AuthForm /></>
+export default function SignIn() {
+  return (
+    <>
+      <AuthForm />
+    </>
+  )
 }
